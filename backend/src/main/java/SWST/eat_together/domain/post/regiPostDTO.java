58494402c6d68f85ac44d202c0247f27e5430d38
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-06-24 13:17:01 +0900

    regiPostDTO 작성

---
+++ backend/src/main/java/SWST/eat_together/domain/post/regiPostDTO.java
@@ -0,0 +1,13 @@
+package SWST.eat_together.domain.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class regiPostDTO {
+    private String title;
+    private String contents;
+}
