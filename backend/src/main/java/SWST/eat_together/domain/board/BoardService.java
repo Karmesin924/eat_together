05a1a5f1f02795421c07c59a45ff8a7d8594a3b7
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-06-25 18:40:22 +0900

    board 페이지 구현

---
+++ backend/src/main/java/SWST/eat_together/domain/board/BoardService.java
@@ -0,0 +1,23 @@
+package SWST.eat_together.domain.board;
+
+import SWST.eat_together.domain.post.Post;
+import SWST.eat_together.domain.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final PostRepository postRepository;
+
+    public Page<Post> getPostsByPage(int pageNumber, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, Sort.by("id").descending());
+        return postRepository.findAll(pageRequest);
+    }
+
+}
