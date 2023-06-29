6f32a356c2ec34d8d1cfd96a3f42e62de274dbcd
Karmesin924 <jinhong0924@naver.com>
2023-06-29 16:25:31 +0900

    같이먹자 필터 구현중 (미완성)

---
+++ frontend/src/pages/FilterDetail.js
@@ -0,0 +1,25 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+const FilterDetail = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleSaveFilters = location.state.handleSaveFilters;
+
+  const handleApplyFilters = () => {
+    const filters = {
+      // 필터 내용을 수집하는 로직 작성
+    };
+    handleSaveFilters(filters);
+    navigate("/LetsEat");
+  };
+
+  return (
+    <div>
+      <h3>필터 상세 설정</h3>
+      {/* 필터 설정 컴포넌트들 */}
+      <button onClick={handleApplyFilters}>적용</button>
+    </div>
+  );
+};
+
+export default FilterDetail;
