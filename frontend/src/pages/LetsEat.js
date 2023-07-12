c103794916c5edefd80706411dd649d85a68bf43
Seoyeon6 <seoyeon8792@gmail.com>
2023-07-13 00:37:59 +0900

    매칭 기본틀 구현

--- frontend/src/pages/LetsEat.js
+++ frontend/src/pages/LetsEat.js
@@ -1,52 +1,71 @@
-import { useNavigate } from "react-router-dom";
-import MyButton from "../components/MyButton";
-import MyHeader from "../components/MyHeader";
-import { useContext, useEffect, useState } from "react";
-import axios from "axios";
-import MyContext from "../components/MyContext";
-import Map from "../components/Map";
+import { useNavigate } from 'react-router-dom';
+import MyButton from '../components/MyButton';
+import MyHeader from '../components/MyHeader';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import MyContext from '../components/MyContext';
+import Map from '../components/Map';
 
 const LetsEat = () => {
   const navigate = useNavigate();
-  const [startTime, setStartTime] = useState("");
+  const [startTime, setStartTime] = useState('');
   const [currentLocation, setCurrentLocation] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [mapLoaded, setMapLoaded] = useState(false);
 
-  const { people, gender, age, menu, conversation, handleSaveFilters } =
-    useContext(MyContext);
+  const { people, gender, age, menu, conversation, handleSaveFilters } = useContext(MyContext);
 
   const handleStartTime = (event) => {
     setStartTime(event.target.value);
   };
 
   const handleMatching = () => {
-    alert("matching start!!");
-    console.log("Selected Filters:", {
+    alert('matching start!!');
+    console.log('Selected Filters:', {
       people,
       gender,
       age,
       menu,
       conversation,
     });
+
+    const filters = {
+      people,
+      gender,
+      age,
+      menu,
+      conversation,
+    };
+
+    axios
+      .post('/api/matching/start', filters)
+      .then((response) => {
+        // 필터 선택 값들을 성공적으로 백엔드로 전송한 후 수행할 작업
+        console.log('필터 선택 값들을 백엔드로 전송했습니다.', response.data);
+        navigate('/Matching');
+      })
+      .catch((error) => {
+        // API 요청 중 오류 발생 시 처리
+        console.log('필터 선택 값을 백엔드로 전송하는데 실패했습니다.', error);
+      });
   };
 
   const handleShowFilter = () => {
     axios
-      .get("/api/filters")
+      .get('/api/filters')
       .then((res) => {
         const filters = res.data;
         handleSaveFilters(filters);
       })
       .catch((err) => {
-        alert("기존 선택항목을 불러오는데 오류가 발생했습니다.");
+        alert('기존 선택항목을 불러오는데 오류가 발생했습니다.');
         console.log(err);
       });
   };
 
   const handleSelectFilter = () => {
-    navigate("/FilterDetail");
+    navigate('/FilterDetail');
   };
 
   const getCurrentLocation = () => {
@@ -62,25 +81,17 @@ const LetsEat = () => {
           setLongitude(longitude);
           setMapLoaded(true); // 위치 정보가 정확하게 가져와지면 map을 로드할 수 있도록 상태 업데이트
         } else {
-          navigator.geolocation.getCurrentPosition(
-            successCallback,
-            errorCallback,
-            options
-          );
+          navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
         }
       };
 
       const errorCallback = (error) => {
-        console.log("위치 정보를 가져오는데 실패했습니다.", error);
+        console.log('위치 정보를 가져오는데 실패했습니다.', error);
       };
 
-      navigator.geolocation.getCurrentPosition(
-        successCallback,
-        errorCallback,
-        options
-      );
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
     } else {
-      console.log("Geolocation API가 지원되지 않습니다.");
+      console.log('Geolocation API가 지원되지 않습니다.');
     }
   };
 
@@ -90,15 +101,15 @@ const LetsEat = () => {
 
   useEffect(() => {
     axios
-      .get("/users/validate")
+      .get('/users/validate')
       .then((res) => {
         if (res.status === 404) {
-          alert("로그인 정보가 없습니다. 다시 로그인해주세요.");
-          navigate("/SignIn");
+          alert('로그인 정보가 없습니다. 다시 로그인해주세요.');
+          navigate('/SignIn');
         }
       })
       .catch((err) => {
-        console.log("로그인이 되어있습니다.");
+        console.log('로그인이 되어있습니다.');
       });
   }, [navigate]);
 
@@ -106,9 +117,7 @@ const LetsEat = () => {
     const now = new Date();
     const nearestHour = Math.ceil(now.getMinutes() / 60) + now.getHours();
 
-    const formattedStartTime = `${
-      nearestHour < 10 ? "0" : ""
-    }${nearestHour}:00`;
+    const formattedStartTime = `${nearestHour < 10 ? '0' : ''}${nearestHour}:00`;
 
     setStartTime(formattedStartTime);
   }, []);
@@ -117,12 +126,12 @@ const LetsEat = () => {
     <div>
       <div>
         <MyHeader
-          headText={"같이 먹자"}
+          headText={'같이 먹자'}
           leftChild={
             <MyButton
-              text={"뒤로가기"}
+              text={'뒤로가기'}
               onClick={() => {
-                navigate("/");
+                navigate('/');
               }}
             />
           }
@@ -137,18 +146,16 @@ const LetsEat = () => {
                 type="text"
                 placeholder="닉네임"
                 value={new Date()
-                  .toLocaleDateString("ko-KR", {
-                    year: "numeric",
-                    month: "2-digit",
-                    day: "2-digit",
-                    weekday: "short",
+                  .toLocaleDateString('ko-KR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    weekday: 'short',
                   })
-                  .replace(/\./g, " /")}
+                  .replace(/\./g, ' /')}
                 readOnly
               />
-              <p className=" text-gray-500 pb-3">
-                ※모임 날짜는 당일로 고정됩니다.
-              </p>
+              <p className=" text-gray-500 pb-3">※모임 날짜는 당일로 고정됩니다.</p>
             </div>
             <div className="flex flex-col justify-center items-center m-auto w-64 flex-shrink-0">
               <p className="font-bold text-3xl pt-3 pb-3">시간 선택</p>
@@ -165,9 +172,9 @@ const LetsEat = () => {
           <div className="flex flex-col justify-center items-center m-auto border-4 border-project rounded-xl w-1/2 mt-5">
             <p className="font-bold text-3xl pt-3 pb-3">필터 선택</p>
             <div>
-              <MyButton text={"불러오기"} onClick={handleShowFilter} />
+              <MyButton text={'불러오기'} onClick={handleShowFilter} />
               <span className="m-1" />
-              <MyButton text={"필터선택"} onClick={handleSelectFilter} />
+              <MyButton text={'필터선택'} onClick={handleSelectFilter} />
             </div>
             <p className="pt-3 text-xl font-semibold pb-2">현재 선택한 필터</p>
             <div className="text-xl pb-3">
@@ -185,33 +192,23 @@ const LetsEat = () => {
               </p>
               <p>
                 <span className="font-bold">메뉴: </span>
-                <span className=" text-red-500 font-semibold">
-                  {menu.join(", ")}
-                </span>
+                <span className=" text-red-500 font-semibold">{menu.join(', ')}</span>
               </p>
               <p>
                 <span className="font-bold">대화빈도: </span>
-                <span className=" text-red-500 font-semibold">
-                  {conversation}
-                </span>
+                <span className=" text-red-500 font-semibold">{conversation}</span>
               </p>
             </div>
           </div>
           <div className="flex flex-col justify-center items-center m-auto border-4 border-project rounded-xl w-1/2 mt-5">
             <p className="font-bold text-3xl pt-3 pb-3">현재 위치</p>
-            <p className=" text-gray-500 pb-3">
-              ※현재 위치 기준으로 근처 인원이 매칭됩니다 :)
-            </p>
+            <p className=" text-gray-500 pb-3">※현재 위치 기준으로 근처 인원이 매칭됩니다 :)</p>
             {latitude && longitude ? (
               <div>
                 <p>
-                  위도: {latitude}, 경도: {longitude},{" "}
+                  위도: {latitude}, 경도: {longitude},{' '}
                 </p>
-                {mapLoaded ? (
-                  <Map latitude={latitude} longitude={longitude} />
-                ) : (
-                  <p>지도를 로딩 중입니다...</p>
-                )}
+                {mapLoaded ? <Map latitude={latitude} longitude={longitude} /> : <p>지도를 로딩 중입니다...</p>}
               </div>
             ) : (
               <p>위치 정보를 가져오는 중...</p>
@@ -219,7 +216,7 @@ const LetsEat = () => {
             <p className="pb-3" />
           </div>
           <p className="pb-3" />
-          <MyButton text={"매칭 시작!!"} onClick={handleMatching} />
+          <MyButton text={'매칭 시작'} onClick={handleMatching} />
         </div>
       </div>
     </div>
---
+++ frontend/src/pages/Macthing.js
@@ -0,0 +1,135 @@
+import { useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import axios from 'axios';
+import MyButton from '../components/MyButton';
+import MyHeader from '../components/MyHeader';
+import MyContext from '../components/MyContext';
+
+const Matching = () => {
+  const navigate = useNavigate();
+  const [matchedUsers, setMatchedUsers] = useState([]); // 백에서주는 매칭유저 정보? 배열로 받음
+  const [matchedCount, setMatchedCount] = useState(0); // 매칭된 사용자 수
+  const [socket, setSocket] = useState(null);
+  const [matchingLimit, setMatchingLimit] = useState(5);
+  const { people } = useContext(MyContext);
+
+  useEffect(() => {
+    // 백엔드와 소켓 연결 설정
+    const newSocket = io('localhost:8080');
+
+    // 연결 성공 시 이벤트 핸들러
+    newSocket.on('connect', () => {
+      console.log('소켓 연결 성공');
+    });
+
+    // 백엔드에서 매칭 정보를 받아오는 이벤트 핸들러
+    newSocket.on('matchingData', (data) => {
+      setMatchedUsers(data);
+      setMatchedCount(data.length);
+    });
+
+    // 연결 해제 시 이벤트 핸들러
+    newSocket.on('disconnect', () => {
+      console.log('소켓 연결 해제');
+    });
+
+    setSocket(newSocket);
+
+    // 컴포넌트 언마운트 시 소켓 연결 해제
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    // 일정한 간격으로 백엔드에 요청 보내기
+    const interval = setInterval(() => {
+      if (socket) {
+        socket.emit('requestMatchingData');
+      }
+    }, 5000); // 5초마다 요청 보냄
+
+    return () => {
+      clearInterval(interval); // 컴포넌트 언마운트 시 요청 간격 해제
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (people === '2인') {
+      setMatchingLimit(2);
+    } else if (people === '3인') {
+      setMatchingLimit(3);
+    } else if (people === '4인') {
+      setMatchingLimit(4);
+    } else {
+      setMatchingLimit(5); //아 상관없음은 또 어떻게 해야하냐 미친~
+    }
+  }, [people]);
+
+  useEffect(() => {
+    if (matchedUsers.length === matchingLimit) {
+      alert('매칭이 완료되었습니다.');
+      setTimeout(() => {
+        navigate('/ChatList');
+      }, 2000); // 2초 후에 페이지 이동
+    }
+  }, [matchedUsers, matchingLimit, navigate]);
+
+  // useEffect(() => {
+  //   // 테스트용 더미 데이터 생성
+  //   const dummyData = [
+  //     { id: 1, name: 'User 1' },
+  //     { id: 2, name: 'User 2' },
+  //     { id: 3, name: 'User 3' },
+  //     { id: 4, name: 'User 4' },
+  //     // { id: 5, name: 'User 5' },
+  //   ];
+
+  //   setMatchedUsers(dummyData);
+  //   setMatchedCount(dummyData.length);
+  // }, []);
+
+  return (
+    <div>
+      <MyHeader
+        headText={'매칭 중'}
+        leftChild={
+          <MyButton
+            text={'뒤로가기'}
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+        }
+      />
+      <h1>매칭 결과</h1>
+      <p>매칭된 사용자 수: {matchedCount}</p>
+
+      {/* 회색 동그라미를 보여주는 부분 */}
+      <div>
+        {Array.from({ length: matchingLimit }, (_, index) => (
+          <div
+            key={index}
+            style={{
+              width: '20px',
+              height: '20px',
+              borderRadius: '50%',
+              backgroundColor: index < matchedCount ? 'orange' : 'gray',
+              margin: '5px',
+            }}
+          ></div>
+        ))}
+      </div>
+
+      {/* 매칭된 사용자 목록을 보여주는 부분 */}
+      <ul>
+        {matchedUsers.map((user) => (
+          <li key={user.id}>{user.name}</li> // 이부분 우리거에 맞게 수정해야 될듯?
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Matching;
