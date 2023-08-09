7592b672866df059c76c5273e49480acaca46053
udonehn <udonehn@gmail.com>
2023-08-10 02:02:26 +0900

    로그인 폼 수정

--- Django_MVT_chatting/accounts/views.py
+++ Django_MVT_chatting/accounts/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
+from django.views.generic import FormView
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
-from accounts.forms import CustomAuthenticationForm
+from accounts.forms import LoginForm
 from accounts.serializers import UserSerializer
 
 
@@ -22,14 +24,24 @@ class SignupAPIView(CreateAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ChatLoginView(LoginView):
+class ChatLoginView(FormView):
     template_name = "partials/form.html"
-    authentication_form = CustomAuthenticationForm
-    extra_context = {
-        "form_name": "로그인",
-        "submit_label": "로그인"
-    }
+    form_class = LoginForm
+    success_url = '/'
 
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+
+        user = authenticate(self.request, username=username, password=password)
+
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            form.add_error(None, "이메일, 닉네임 또는 비밀번호가 올바르지 않습니다.")
+            return self.form_invalid(form)
 
 logout = LogoutView.as_view(
     next_page="accounts:login",
