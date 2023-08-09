f6bfed3a9392a0e2b6f03313d51b06557e46a793
Seoyeon6 <seoyeon8792@gmail.com>
2023-08-09 17:44:23 +0900

    회원가입 닉네임, 이메일 중복 확인 및 닉네임 자음, 모음만 사용 불가

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,24 +1,24 @@
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import MyHeader from '../components/MyHeader';
+import MyButton from '../components/MyButton';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [birth, setBirth] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [gender, setGender] = useState("");
-  const [nameMessage, setNameMessage] = useState("");
-  const [emailMessage, setEmailMessage] = useState("");
-  const [birthMessage, setBirthMessage] = useState("");
-  const [nicknameMessage, setNicknameMessage] = useState("");
-  const [passwordMessage, setPasswordMessage] = useState("");
-  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [birth, setBirth] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [gender, setGender] = useState('');
+  const [nameMessage, setNameMessage] = useState('');
+  const [emailMessage, setEmailMessage] = useState('');
+  const [birthMessage, setBirthMessage] = useState('');
+  const [nicknameMessage, setNicknameMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
   const [isName, setIsName] = useState(false);
   const [isEmail, setIsEmail] = useState(false);
   const [isBirth, setIsBirth] = useState(false);
@@ -28,28 +28,12 @@ const SignUp = () => {
   const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
   const checkButtonEnabled = useCallback(() => {
-    if (
-      isName &&
-      isEmail &&
-      isBirth &&
-      isNickname &&
-      isPassword &&
-      isPasswordConfirm &&
-      gender !== ""
-    ) {
+    if (isName && isEmail && isBirth && isNickname && isPassword && isPasswordConfirm && gender !== '') {
       setIsButtonEnabled(true);
     } else {
       setIsButtonEnabled(false);
     }
-  }, [
-    isName,
-    isEmail,
-    isBirth,
-    isNickname,
-    isPassword,
-    isPasswordConfirm,
-    gender,
-  ]);
+  }, [isName, isEmail, isBirth, isNickname, isPassword, isPasswordConfirm, gender]);
 
   useEffect(() => {
     checkButtonEnabled();
@@ -60,10 +44,10 @@ const SignUp = () => {
     setName(currentName);
 
     if (currentName.length < 2 || currentName.length > 5) {
-      setNameMessage("이름은 2글자 이상 5글자 이하로 입력해주세요");
+      setNameMessage('이름은 2글자 이상 5글자 이하로 입력해주세요');
       setIsName(false);
     } else {
-      setNameMessage("올바른 형식입니다");
+      setNameMessage('올바른 형식입니다');
       setIsName(true);
     }
 
@@ -73,14 +57,13 @@ const SignUp = () => {
   const onChangeEmail = (e) => {
     const currentEmail = e.target.value;
     setEmail(currentEmail);
-    const emailRegExp =
-      /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+    const emailRegExp = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
 
     if (!emailRegExp.test(currentEmail)) {
-      setEmailMessage("이메일의 형식이 올바르지 않습니다");
+      setEmailMessage('이메일의 형식이 올바르지 않습니다');
       setIsEmail(false);
     } else {
-      setEmailMessage("사용 가능한 이메일입니다");
+      setEmailMessage('사용 가능한 이메일입니다');
       setIsEmail(true);
     }
 
@@ -92,7 +75,7 @@ const SignUp = () => {
     setBirth(currentBirth);
 
     if (!currentBirth) {
-      setBirthMessage("생년월일을 선택해주세요");
+      setBirthMessage('생년월일을 선택해주세요');
       setIsBirth(false);
     } else {
       setIsBirth(true);
@@ -105,11 +88,16 @@ const SignUp = () => {
     const currentNickname = e.target.value;
     setNickname(currentNickname);
 
+    const hangulConsonantVowelRegExp = /^[ㄱ-ㅎㅏ-ㅣ]+$/;
+
     if (currentNickname.length < 2 || currentNickname.length > 8) {
-      setNicknameMessage("닉네임은 2글자 이상 8글자 이하로 입력해주세요");
+      setNicknameMessage('닉네임은 2글자 이상 8글자 이하로 입력해주세요');
+      setIsNickname(false);
+    } else if (hangulConsonantVowelRegExp.test(currentNickname)) {
+      setNicknameMessage('닉네임에 한글 자음 또는 모음만을 사용할 수 없습니다');
       setIsNickname(false);
     } else {
-      setNicknameMessage("올바른 형식입니다");
+      setNicknameMessage('올바른 형식입니다');
       setIsNickname(true);
     }
 
@@ -121,10 +109,10 @@ const SignUp = () => {
     setPassword(currentPassword);
 
     if (currentPassword.length < 6) {
-      setPasswordMessage("비밀번호는 6자리 이상으로 입력해주세요");
+      setPasswordMessage('비밀번호는 6자리 이상으로 입력해주세요');
       setIsPassword(false);
     } else {
-      setPasswordMessage("올바른 형식입니다");
+      setPasswordMessage('올바른 형식입니다');
       setIsPassword(true);
 
       if (currentPassword === passwordConfirm) {
@@ -141,10 +129,10 @@ const SignUp = () => {
     setPasswordConfirm(currentPasswordConfirm);
 
     if (currentPasswordConfirm !== password) {
-      setPasswordConfirmMessage("비밀번호가 일치하지 않습니다");
+      setPasswordConfirmMessage('비밀번호가 일치하지 않습니다');
       setIsPasswordConfirm(false);
     } else {
-      setPasswordConfirmMessage("비밀번호가 일치합니다");
+      setPasswordConfirmMessage('비밀번호가 일치합니다');
       setIsPasswordConfirm(true);
     }
 
@@ -168,14 +156,31 @@ const SignUp = () => {
     };
 
     axios
-      .post("/users/signup", userData, { withCredentials: true })
+      .post('/users/signup', userData, { withCredentials: true })
       .then((response) => {
-        console.log(response.data);
-        alert("회원가입이 완료되었습니다.");
-        navigate("/SignIn"); // 가입 성공 시 이동할 경로를 설정해주세요
+        alert('회원가입이 완료되었습니다.');
+        navigate('/SignIn'); // 가입 성공 시 이동할 경로를 설정해주세요
       })
       .catch((error) => {
-        alert("오류로 인해 회원가입이 완료되지 않았습니다.");
+        // 서버에서 에러 응답을 받았을 경우 처리
+        if (error.response && error.response.status === 400) {
+          let errorMessage = '';
+
+          // 이메일 중복 체크
+          if (error.response.data.email) {
+            errorMessage += '이메일 중복입니다.';
+          }
+
+          // 닉네임 중복 체크
+          if (error.response.data.nickname) {
+            errorMessage += '닉네임 중복입니다.';
+          }
+
+          alert(errorMessage);
+        } else {
+          // 다른 에러
+          alert('오류로 인해 회원가입이 완료되지 않았습니다.');
+        }
         console.error(error);
       });
   };
@@ -183,10 +188,10 @@ const SignUp = () => {
   return (
     <div className="signup">
       <MyHeader
-        headText={"회원가입"}
+        headText={'회원가입'}
         leftChild={
           <MyButton
-            text={"뒤로가기"}
+            text={'뒤로가기'}
             onClick={() => {
               navigate(-1);
             }}
@@ -205,9 +210,7 @@ const SignUp = () => {
             value={name}
             onChange={onChangeName}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {nameMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{nameMessage}</span>
 
           <label className="text-xl font-bold" htmlFor="email">
             이메일
@@ -219,32 +222,18 @@ const SignUp = () => {
             value={email}
             onChange={onChangeEmail}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {emailMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{emailMessage}</span>
 
           <label className="text-xl font-bold" htmlFor="gender">
             성별
           </label>
           <div className="font-semibold text-xl flex gap-4">
             <label htmlFor="male">
-              <input
-                type="radio"
-                id="male"
-                value="man"
-                checked={gender === "man"}
-                onChange={onChangeGender}
-              />
+              <input type="radio" id="male" value="man" checked={gender === 'man'} onChange={onChangeGender} />
               남성
             </label>
             <label htmlFor="female">
-              <input
-                type="radio"
-                id="female"
-                value="woman"
-                checked={gender === "woman"}
-                onChange={onChangeGender}
-              />
+              <input type="radio" id="female" value="woman" checked={gender === 'woman'} onChange={onChangeGender} />
               여성
             </label>
           </div>
@@ -259,9 +248,7 @@ const SignUp = () => {
             value={birth}
             onChange={onChangeBirth}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {birthMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{birthMessage}</span>
 
           <label className="text-xl font-bold" htmlFor="nickname">
             닉네임
@@ -273,9 +260,7 @@ const SignUp = () => {
             value={nickname}
             onChange={onChangeNickname}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {nicknameMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{nicknameMessage}</span>
 
           <label className="text-xl font-bold" htmlFor="password">
             비밀번호
@@ -287,9 +272,7 @@ const SignUp = () => {
             value={password}
             onChange={onChangePassword}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {passwordMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{passwordMessage}</span>
 
           <label className="text-xl font-bold" htmlFor="passwordConfirm">
             비밀번호 확인
@@ -301,15 +284,9 @@ const SignUp = () => {
             value={passwordConfirm}
             onChange={onChangePasswordConfirm}
           />
-          <span className="text-sm font-semibold text-signupmessage">
-            {passwordConfirmMessage}
-          </span>
+          <span className="text-sm font-semibold text-signupmessage">{passwordConfirmMessage}</span>
 
-          <MyButton
-            text="가입하기"
-            onClick={onClickSignUp}
-            disabled={!isButtonEnabled}
-          />
+          <MyButton text="가입하기" onClick={onClickSignUp} disabled={!isButtonEnabled} />
         </div>
       </div>
     </div>
