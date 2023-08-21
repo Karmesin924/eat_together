4ad84ea0e86f73a5da06505b7adf39deb135b263
Seoyeon6 <seoyeon8792@gmail.com>
2023-08-21 16:28:46 +0900

    매칭 취소 기능 및 매칭 이탈 시 경고

--- frontend/src/pages/Matching.js
+++ frontend/src/pages/Matching.js
@@ -12,7 +12,7 @@ const Matching = () => {
   const [matchedUsers, setMatchedUsers] = useState([]);
   const [matchingComplete, setMatchingComplete] = useState(false);
   const [roomPk, setRoomPk] = useState(null);
-  const [isUserInList, setIsUserInList] = useState(false); // Added state to track if user is in the list
+  const [isUserInList, setIsUserInList] = useState(false);
   const [matchingFailed, setMatchingFailed] = useState(false);
   const { nickname, people, gender, age, menu, startTime, conversation, latitude, longitude } = useContext(MyContext);
 
@@ -27,6 +27,21 @@ const Matching = () => {
     longitude,
     startTime,
     nickname,
+  };
+
+  // 페이지 이탈 시 경고 이벤트 : beforeunload
+  const handleBeforeUnload = (event) => {
+    event.preventDefault();
+    event.returnValue = '매칭이 취소 됩니다. 정말로 이 페이지를 떠나시겠습니까?';
+  };
+
+  // 뒤로 가기 버튼을 눌렀을 시 beforeunload 이벤트
+  const handleGoBack = () => {
+    const isConfirmed = window.confirm('매칭이 취소됩니다. 이 페이지에서 나가시겠습니까?');
+    if (isConfirmed) {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      navigate('/LetsEat');
+    }
   };
 
   useEffect(() => {
@@ -49,7 +64,7 @@ const Matching = () => {
             setMatchingComplete(true);
             setMatchedUsers(data.nickname);
             setRoomPk(data.roomPk);
-            setIsUserInList(data.nickname.includes(nickname)); // Check if user is in the list
+            setIsUserInList(data.nickname.includes(nickname));
             stompClient.deactivate();
             console.log('매칭 완료 및 소켓 연결 해제');
           } else if (data.type === 'matching_failed' && data.nickname === nickname) {
@@ -58,7 +73,8 @@ const Matching = () => {
             console.log('매칭 실패 및 소켓 연결 해제');
           } else if (data.type === 'matching_already_exist' && data.nickname === nickname) {
             alert('이미 매칭 중입니다');
-          } console.log('이미 매칭 중');
+          }
+          console.log('이미 매칭 중');
         } catch (error) {
           console.error('Error parsing message:', error);
         }
@@ -72,30 +88,59 @@ const Matching = () => {
 
     stompClient.activate();
 
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
     return () => {
       stompClient.deactivate();
+      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
 
+  const handleCancelMatching = () => {
+    const isConfirmed = window.confirm('매칭을 취소하시겠습니까?');
+    if (isConfirmed) {
+      const cancelData = {
+        type: 'matching_cancelled',
+        nickname,
+      };
+
+      const stompClient = new Client({
+        webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+        debug: (str) => {
+          console.log(str);
+        },
+        reconnectDelay: 5000,
+      });
+
+      stompClient.onConnect = () => {
+        stompClient.publish({
+          destination: '/app/matching/start',
+          body: JSON.stringify(cancelData),
+        });
+        stompClient.deactivate();
+      };
+
+      stompClient.activate();
+
+      navigate('/LetsEat');
+    }
+  };
+
   return (
     <div>
-      <MyHeader
-        headText={'매칭 중'}
-        leftChild={
-          <MyButton
-            text={'뒤로가기'}
-            onClick={() => {
-              navigate(-1);
-            }}
-          />
-        }
-      />
+      <MyHeader headText={'매칭 중'} leftChild={<MyButton text={'뒤로가기'} onClick={handleGoBack} />} />
+
       {matchingComplete && isUserInList ? (
         <MatchComplete matchedUsers={matchedUsers} roomPk={roomPk} />
       ) : matchingFailed ? (
         <MatchFailed />
       ) : (
-        <Loading />
+        <>
+          <Loading />
+          <div className="flex justify-center mt-5">
+            <MyButton text="매칭 취소" onClick={handleCancelMatching} />
+          </div>
+        </>
       )}
     </div>
   );
